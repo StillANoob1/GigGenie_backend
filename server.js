a073fb1ab85eb56aded2eb877f477336fdc32250
Anas <anas241124@gmail.com>
2023-04-14 10:20:37 +0530

    set cors

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ const connect = ()=>{
     })
 }
 
-app.use(cors({ origin:"http://127.0.0.1:5173" , credentials:true }));
+app.use(cors({ origin:true , credentials:true }));
 app.use(express.json());
 app.use(cookieParser());
 
